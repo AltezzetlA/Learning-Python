dde2972969f270b2657bc8224d452e72db2b74ae
Cameron Gillespie <cameronvgillespie@gmail.com>
2025-05-24 17:39:58 +0900

    renamed files and fixed Game of Life

---
+++ ConwayGameOfLifeV2.py
@@ -0,0 +1,6 @@
+
+""""This is Conway's Game of Life"""
+
+class Grid:
+    def __init__(self):
+        pass                                 
--- ConwaysGameOfLife.py
+++ ConwaysGameOfLife.py
@@ -2,50 +2,50 @@ import sys, random, time
 #Conway's Game of Life
 
 #set the staring grid size
-WIDTH = 50
-HEIGHT = 50
+WIDTH = 30
+HEIGHT = 30
 grid = []
 #define the staring living and dead cells grid
 for x in range(WIDTH):
     column = []
     for y in range(HEIGHT):
-        if random.randint(0, 1) == 1:
-            column.append('#') #append living cell
+        if random.randint(0, 10) < 3:
+            column.append('██') #append living cell
         else:
-            column.append(' ')
+            column.append('  ')
     grid.append(column) #append dead cell
 
 
 def checkNeighboringCells():
     global WIDTH, HEIGHT, grid
-    for x in range(WIDTH - 1):    
-        for y in range(HEIGHT - 1):
+    for x in range(WIDTH):    
+        for y in range(HEIGHT):
             aliveNeighborCount = 0  
             if x > 0:
-                if grid[x - 1][y] == '#': #check neighboring cell to the West
+                if grid[x - 1][y] == '██': #check neighboring cell to the West
                     aliveNeighborCount += 1
-            if x < WIDTH:
-                if grid[x + 1][y] == '#': #check neighboring cell to the East
+            if x < WIDTH - 1:
+                if grid[x + 1][y] == '██': #check neighboring cell to the East
                     aliveNeighborCount += 1
             if y > 0:
-                if grid[x][y - 1] == '#': #check neighboring cell North
+                if grid[x][y - 1] == '██': #check neighboring cell North
                         aliveNeighborCount += 1
-            if y < HEIGHT:
-                if grid[x][y + 1] == '#': #check neighboring cell South
+            if y < HEIGHT - 1:
+                if grid[x][y + 1] == '██': #check neighboring cell South
                         aliveNeighborCount += 1
             if x > 0 and y > 0: 
-                if grid[x - 1][y - 1] == '#': #check neighboring cell Northwest
+                if grid[x - 1][y - 1] == '██': #check neighboring cell Northwest
                         aliveNeighborCount += 1    
-            if x < WIDTH and y > 0:    
-                if grid[x + 1][y - 1] == '#': #check neighboring cell Northeast
+            if x < WIDTH - 1 and y > 0:    
+                if grid[x + 1][y - 1] == '██': #check neighboring cell Northeast
                         aliveNeighborCount += 1   
-            if x > 0 and y <  HEIGHT:   
-                if grid[x - 1][y + 1] == '#': #check neighboring cell Southwest
+            if x > 0 and y <  HEIGHT -1:   
+                if grid[x - 1][y + 1] == '██': #check neighboring cell Southwest
                         aliveNeighborCount += 1  
-            if x < WIDTH and y < HEIGHT: 
-                if grid[x + 1][y + 1] == '#': #check neighboring cell Southeast
+            if x < WIDTH - 1 and y < HEIGHT -1: 
+                if grid[x + 1][y + 1] == '██': #check neighboring cell Southeast
                         aliveNeighborCount += 1  
-            if grid[x][y] == '#':
+            if grid[x][y] == '██':
                 cellCondition = ['alive', aliveNeighborCount, x, y,] #populate information about the cell
             else: 
                 cellCondition = ['dead', aliveNeighborCount, x, y,] #populate information about the cell
@@ -57,19 +57,20 @@ def deadOrAlive(): #generate the next grid
     genList = list(checkNeighboringCells()) #compile the data for each cell into one list
     for i in range(len(genList)):
         if genList[i][0] == 'alive' and genList[i][1] != 2 and genList[i][1] != 3: #define conditions for a living cell to die
-            grid[genList[i][2]][genList[i][3]] = ' '
+            grid[genList[i][2]][genList[i][3]] = '  '
         elif genList[i][0] == 'dead' and genList[i][1] == 3: #define condition to revive cell
-            grid[genList[i][2]][genList[i][3]] = '#'
+            grid[genList[i][2]][genList[i][3]] = '██'
 
 def printGrid():
-     for x in range(WIDTH - 1):
+    for x in range(WIDTH - 1):
         for y in range(HEIGHT - 1):
                print(grid[x][y], end='')
         print()
+    print("--------------------------------------")
 
 
 while True:
     printGrid()
     checkNeighboringCells() 
     deadOrAlive() #generate the next grid
-    time.sleep(2)
+    time.sleep(0.5)
